f97ab05bafb67faac5df9ea90a3ff21d1aec104b
marwitz93j <37061272+marwitz93j@users.noreply.github.com>
2020-02-11 16:47:36 -0600

    Just Bc

---
+++ pcapplusplus-19.12-ubuntu-16.04-gcc-5.4.0/mk/PcapPlusPlus.mk
@@ -0,0 +1,33 @@
+PCAPPLUSPLUS_HOME := /PcapPlusPlus/Home/Dir
+
+### COMMON ###
+
+# includes
+PCAPPP_INCLUDES := -I$(PCAPPLUSPLUS_HOME)/header
+
+# libs dir
+PCAPPP_LIBS_DIR := -L$(PCAPPLUSPLUS_HOME)/
+
+# libs
+PCAPPP_LIBS := -lPcap++ -lPacket++ -lCommon++
+
+# post build
+PCAPPP_POST_BUILD :=
+
+# build flags
+PCAPPP_BUILD_FLAGS :=
+
+ifdef PCAPPP_ENABLE_CPP_FEATURE_DETECTION
+	PCAPPP_BUILD_FLAGS += -DPCAPPP_CPP_FEATURE_DETECTION -std=c++11 
+endif
+### LINUX ###
+
+# includes
+PCAPPP_INCLUDES += -I/usr/include/netinet
+
+# libs
+PCAPPP_LIBS += -lpcap -lpthread
+
+# allow user to add custom LDFLAGS
+PCAPPP_BUILD_FLAGS += $(LDFLAGS)
+
